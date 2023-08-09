aef716f1a691d4cb4e92ae13e0146eb89c98d44a
NaJ <naj10000@live.com>
2023-08-09 14:17:48 -0400

    mvp met

--- src/components/Character.js
+++ src/components/Character.js
@@ -2,9 +2,25 @@
 import React from "react";  
 import styled from "styled-components";
 
+const Div = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 2%;
+  border: 1px solid white;
+  background-color: black;
+  width: 30%;
+  color: orange;
+`
+
+
 const Character = (props) => {
+   console.log(props)
     return (
-        <div>Character</div>
+        <Div>
+          <h2>Name: {props.chars.name}</h2> 
+          <h3>Height: {props.chars.height}</h3> 
+        </Div>
     )
 }
 
